27339f1da1bfafa37bd8ecec83d322f8fb72b7ef
Satyadev Saini <105744852+SatyadevSaini@users.noreply.github.com>
2023-05-19 16:57:08 +0530

    Form to NewExpence to App.js Data send
    Form to NewExpence to App.js Data send  data send also with here we will also send the  details also 
    
    parent  
      props.onDataSave(expenceData); 
    
    Child 
    <ExpenceForm  onDataSave = {SaveNewExpenceDataHere}  />   this will be involked  SaveNewExpenceDataHere

--- ExpenseForm.js
+++ ExpenseForm.js
@@ -1,70 +1,79 @@
 //import { data } from "../../App";
 
 import React from "react";
-//import DateFormat from "./Expence/DateFormat";
+// import DateFormat from "./Expence/DateFormat";
 import "./ExpenceForm.css";
 import { useState } from "react";
 
 
-const MyForm = () =>{
+const MyForm = (props) => {
 
     // manage the Multiple useState here ..
 
-    // const[setTitleHere , setUpdatedTitile] = useState(" ");
-    // const[setpriceHere , setUpdatedPrice] = useState(" ");
-    // const[setDateHere , setUpdatedDate] = useState(" ");
+    const[setTitleHere , setUpdatedTitile] = useState(" ");
+    const[setpriceHere , setUpdatedPrice] = useState(" ");
+    const[setDateHere , setUpdatedDate] = useState(" ");
 
     // now Do It with only one State using the Object here .....
     
-    const[setDataHere , setUpdatedData] = useState(
-        {
-            Entertitle:"" , 
-            Enterprice:"",
-            Enterdate:""
+    // const[setDataHere , setUpdatedData] = useState(
+    //     {
+    //         Entertitle:"" , 
+    //         Enterprice:"",
+    //         Enterdate:""
 
-        }
-    );
+    //     }
+    // );
 
     
 
 
     const  getTitleHere = (event) =>{
-        //  setUpdatedTitile(event.target.value);
-        setUpdatedData( (prevValue) => {
-          return{ ...prevValue , Entertitle:event.target.value}   
-        });
+         setUpdatedTitile(event.target.value);
+        // setUpdatedData( (prevValue) => {
+        //   return{ ...prevValue , Entertitle:event.target.value}   
+        // });
     }
 
     const  getPriceHere = (event) =>{
-        // setUpdatedPrice(event.target.value);
-        setUpdatedData( (prevValue) => {
-            return{ ...prevValue , Enterprice:event.target.value}   
-          });
+        setUpdatedPrice(event.target.value);
+        // setUpdatedData( (prevValue) => {
+        //     return{ ...prevValue , Enterprice:event.target.value}   
+        //   });
     }
 
     const  getDateHere = (event) =>{
-        // setUpdatedDate(event.target.value);
-        setUpdatedData( (prevValue) => {
-            return{ ...prevValue , Enterdate:event.target.value}   
-          });
+        setUpdatedDate(event.target.value);
+        // setUpdatedData( (prevValue) => {
+        //     return{ ...prevValue , Enterdate:event.target.value}   
+        //   });
     }
 
     //Form Submit Hnadler here .
+
     const SubmitFormHandler = (event) => {
        event.preventDefault();
 
    const expenceData =  
     {
-        prices : setDataHere.Enterprice,
-        titles : setDataHere.Entertitle,
-        dates : new Date(setDataHere.Enterdate)
+        prices : setpriceHere,
+        titles : setTitleHere,
+        dates : new Date(setDateHere)
 
        };
-
-       console.log(expenceData);
-
+      
+       
+       
+      props.onDataSave(expenceData);  // on form submitttion we will send the value from this form to another page ...
+     
+      // console.log(expenceData);
+       // make form empty after the submit  
+       setUpdatedDate("");
+       setUpdatedPrice("");
+       setUpdatedTitile("");
+         
     };
-    
+
     return(
          
         <div className="formdiv">
@@ -74,17 +83,17 @@ const MyForm = () =>{
         <div className="id_title">
     
        <div>
-       <label> TITLE: <input  className="heighttext"  type="text" onChange={getTitleHere}></input></label>
+       <label> TITLE: <input  className="heighttext" value={setTitleHere}  type="text"  onChange={getTitleHere}></input></label>
        </div>   
           </div>
 
         <div className="price_date">
         <div>
-        <label> PRICE: <input  className="heighttext"  type="number" onChange={getPriceHere}></input></label>  
+        <label> PRICE: <input  className="heighttext" value={setpriceHere}   type="number" onChange={getPriceHere}></input></label>  
         </div>
         
         <div>
-        <label> DATE:  </label> <input  className="heighttext"  type="date" min="2020-01-01"   max="2023-01-06"  
+        <label> DATE:  </label> <input  className="heighttext"  value={setDateHere} type="date" min="2020-01-01"   max="2023-01-06"
         onChange={getDateHere}>
         </input>  
         </div>
