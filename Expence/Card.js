07d511f942b0e16fdfbd4dd18d3d4c692414b92f
Satyadev Saini <105744852+SatyadevSaini@users.noreply.github.com>
2023-05-23 11:12:54 +0530

    Filter with No Data
    Here will gave functionality of if we will search for a year if it has no data then print the custom message

---
+++ Expence/Card.js
@@ -0,0 +1,13 @@
+
+import './card.css';
+
+const Card = (props) =>{
+ const classes = 'card ' + props.className;
+
+ return(<div className={classes}>
+    {props.children} 
+ </div> )}
+
+export default Card;
+
+//props.children is used to add the data in JSX. 
---
+++ Expence/ExpenceITEM.js
@@ -0,0 +1,95 @@
+import "./Expence.css";
+import DateFormat from "./DateFormat";
+import Card from "./Card";
+import { useState } from 'react';
+
+
+const Expence = (dat) => {
+
+  const [title , setExpenses] = useState(dat.title);
+  
+  const update = () =>{
+    setExpenses("updated");
+  } 
+
+  //************************Method for Filtering the Data here********************************** USeState will be used ********
+        const [currentYear , updatedYear]  =     useState('2018')    
+
+  const ChangeTheYear = (e) => {
+    console.log("Filtered Elements here")
+    console.log(e.target.value);  // how to get value from the drop down here....
+    updatedYear(e.target.value); // selected year intp update 
+  }
+
+ const filteredYear =   dat.item.filter( (element)  => {
+      return element.date.getFullYear().toString() === currentYear;  // the year which we will select only fiter those 
+    })
+
+
+
+
+    //**************condition for  filtered element is empty or not ********************
+
+    let expenceContent = <h2>NO EXPENCE FOUNDS </h2>;
+
+    if(filteredYear.length > 0 ){
+      expenceContent = filteredYear.map((expence , index) => 
+      <div className="expence-item" key = {expence.id}>
+            
+            <div>
+        
+             <DateFormat
+             date = {expence.date} //its pros here .. 
+             ></DateFormat>
+            </div>
+            <div className="expence-item__description">{expence.title}
+            <div className="expence-item__price">${expence.price}</div></div>
+            
+              {/* addding eventlistner here  */}
+            <button className="changeTitle" onClick={update}>Update</button>
+        
+          </div>
+    
+    )
+    }
+    
+
+
+
+   
+  
+
+  return (
+    <Card className="mainClass">
+
+     <div className="filtermain" >
+    <div className="filter">
+    <h2 >FILTERED BY YEAR   </h2> 
+     </div>
+     
+<div className="filterSelect">
+<select value={currentYear} onChange={ChangeTheYear} >
+<option value="2018">2018</option>
+  <option value="2019">2019</option>
+  <option value="2020">2020</option>
+  <option value="2021">2021</option>
+  <option value="2022">2022</option>
+  <option value="2023">2023</option>
+  <option value="2024">2024</option>
+  
+
+</select>  
+</div>
+ </div> <br></br>
+
+
+{/* Condition Rerendering if there is No element then print the message no expence found  */}
+
+  { expenceContent }  
+
+    </Card>
+  );
+}
+
+export default Expence;
+
